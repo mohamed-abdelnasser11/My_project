c6c5ff632868d7d8794b3707615b8fefe023c16e
abdelnasser <mohamedabdelnasser0099@gmail.com>
2025-08-09 15:00:59 +0300

    add power function

--- calc.py
+++ calc.py
@@ -1,9 +1,8 @@
 def sum_aaaaa(x1,x2):
     return x1+x2
-<<<<<<< HEAD
 def multi_aaa(x1,x2):
     return x1*x2
-=======
 def divi_Aaaa(x1,x2):
     return x1 /x2
->>>>>>> new_feature
+def pwr_aaa(x):
+    return x*x
